bf1fb84c9826913c6cebef07823d65841e23c61e
Troy Christian <apostasy8@gmail.com>
2020-09-08 23:37:19 -0400

    outlets connected

---
+++ Quizzler-iOS13/QuizViewController.swift
@@ -0,0 +1,40 @@
+//
+//  QuizViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Lambda_School_Loaner_219 on 9/8/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    @IBOutlet weak var questionTextLabel: UILabel!
+    
+    @IBAction func answerButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var progressBarView: UIProgressView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
